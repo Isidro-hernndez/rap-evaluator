c33d854250dd74965a3d1d11285cb30d8ad0361f
isiherge <skull_caz0412@hotmail.com>
2019-04-17 19:04:56 -0500

    first commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+	$title = $faker->sentence;
+
+    return [
+        'title' => $title,
+        'slug' => str_slug($title),
+        'excerpt' => $faker->text(300),
+        'body' => $faker->text(850),
+    ];
+});
